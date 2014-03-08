b52b48631b911ab49ceb8e72f073c8f416b7223f
pcsmits <smits2010@gmail.com>
2014-03-08 16:19:20 -0700

    inital commit

--- sbbrg.php
+++ sbbrg.php
@@ -13,21 +13,34 @@
 </head>
 
 <?php
-	$mysql_db = "uwwiscus_ssbrg";
+	$mysql_db = "uwwiscus_sbbrg";
 	$mysql_user = "uwwiscus_sbbrg";
 	$mysql_pass = "sbbrg#@!";
-	$dbconn = mysql_connect("localhost", $mysql_user, $mysql_pass);;
+	$dbconn = mysql_connect("localhost", $mysql_user, $mysql_pass);
+	mysql_select_db($mysql_db, $dbconn);
 
 	$update = $_POST['updatedb'];
 	if (isset($update)){
 		#query db for last inserted date 
 		$sql = "SELECT closing_date FROM stocks WHERE ticker_symbol='AAPL' ORDER BY closing_date DESC LIMIT 1";
-		$result = mysql_query($sql, $dbconn);
+		$result = mysql_query($sql, $dbconn) or die(var_dump(mysql_error()));
 		if(mysql_num_rows($result)) {
-
+			$row = mysql_fetch_row($result);
+			var_dump($row);
+		} else {
+			echo "No rows returned";
 		}
 
 		#query closing price up to the date for 'x' stock
+		$ticker_sym = "AAPL";
+		date_default_timezone_set('UTC');
+		$currDate = date("c");
+		var_dump($currDate);
+
+		$yahoo_base_api = "http://query.yahooapis.com/v1/public/yql";
+		$query = "select * from yahoo.finance.historicaldata where symbol = '".$ticker_sym."' and startDate = '".$startDate."' and endDate = '".$endDate;
+
+		$yql_query = $yahoo_base_api . "?q=" . urlencode($query);
 
 	}
 ?>
