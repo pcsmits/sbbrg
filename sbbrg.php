d2e4375509ea118dcff9e72397494b82cda9e4b9
pcsmits <smits2010@gmail.com>
2014-03-08 17:04:48 -0700

    yahoo api xml parsing

--- sbbrg.php
+++ sbbrg.php
@@ -25,22 +25,41 @@
 		$sql = "SELECT closing_date FROM stocks WHERE ticker_symbol='AAPL' ORDER BY closing_date DESC LIMIT 1";
 		$result = mysql_query($sql, $dbconn) or die(var_dump(mysql_error()));
 		if(mysql_num_rows($result)) {
-			$row = mysql_fetch_row($result);
-			var_dump($row);
+			$startDate = mysql_fetch_row($result);
 		} else {
 			echo "No rows returned";
 		}
 
 		#query closing price up to the date for 'x' stock
+		# To-Do make yahoo api function	 
 		$ticker_sym = "AAPL";
+
+		# Format the current date for the query
 		date_default_timezone_set('UTC');
 		$currDate = date("c");
-		var_dump($currDate);
+		$endDate = explode("T", $currDate);
 
+		# setting up yahoo api call
 		$yahoo_base_api = "http://query.yahooapis.com/v1/public/yql";
-		$query = "select * from yahoo.finance.historicaldata where symbol = '".$ticker_sym."' and startDate = '".$startDate."' and endDate = '".$endDate;
+		$query = "select Close from yahoo.finance.historicaldata where symbol='".$ticker_sym."' and startDate='".$startDate[0]."' and endDate='".$endDate[0]."'";
+
+		var_dump($query);
 
 		$yql_query = $yahoo_base_api . "?q=" . urlencode($query);
+		$yql_query .= "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+		var_dump($yql_query);
+
+
+		# TO-DO make curl Function	
+		$session = curl_init($yql_query);  
+		curl_setopt($session, CURLOPT_RETURNTRANSFER,true);      
+		$json = curl_exec($session);
+		
+		$phpObj =  json_decode($json); 
+		var_dump($phpObj);
+		
+
 
 	}
 ?>
